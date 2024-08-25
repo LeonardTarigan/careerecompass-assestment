ae166675be93a0daf2c3e33154727c8338a86cf3
LeonardTarigan <leontrg32@gmail.com>
2024-08-25 23:00:54 +0700

    feat: adding og tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/ui/header";
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
@@ -9,6 +8,12 @@ const mainFont = Plus_Jakarta_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "DiaLink",
   description: "Web-based phone book application",
+  openGraph: {
+    title: "DiaLink Phone Book App",
+    description: "Phone book application on the web",
+    images:
+      "https://images.unsplash.com/photo-1485770958101-9dd7e4ea6d93?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
 };
 
 export default function RootLayout({
